35b97dc3605f2b77ec20b92c8dfe988da83852cc
kabelo2ka <kabelo@frontrunner.co.za>
2018-11-26 10:32:56 +0200

    Users Table setup

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Kabelo',
+            'email' => 'kabelo@gmail.com',
+            'password' => bcrypt('secret'),
+            'is_admin' => true
+        ]);
+        factory(App\User::class, 20)->create();
+    }
+}
